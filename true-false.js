7c292c5e69b7aa4d82823aaec0bfbe5fece3873c
AhmedKamon <ahmedkamon3@gmail.com>
2021-02-02 00:44:42 +0600

    undefined

--- true-false.js
+++ true-false.js
@@ -1,11 +1,14 @@
 // let name = "kamno";
 // let name = "";
 // let name = "0";
-let name = 0;
+// let name = 0;
 // let name = NaN;
 // let name = "NaN";
 // let name = null;
 // let name = "null";
+// let name = ``;
+let name = "";
+// let name = undefined;
 
 if(name || name == 0){
     console.log("yes, its true");
---
+++ undefindVsNull.js
@@ -0,0 +1,15 @@
+// let kamon;
+// console.log(kamon);
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+const result = add(10, 12);
+console.log(result);
+
+
+
+//**if parameter haven't passed then its undefined
+// if proparty is not sen its undefined */
+// looking for a value in a array or var but there is nothing then its undefined;
+// null is kind of nothing . there is nothing. i can une this to check my code. it kind of undefined 
