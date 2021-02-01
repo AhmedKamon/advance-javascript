0ab696a4ef6e5e61dda6dff20d0c4a4db437523e
AhmedKamon <ahmedkamon3@gmail.com>
2021-02-01 23:53:26 +0600

    trueFalse

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// let name = "kamno";
+// let name = "";
+// let name = "0";
+let name = 0;
+// let name = NaN;
+// let name = "NaN";
+// let name = null;
+// let name = "null";
+
+if(name || name == 0){
+    console.log("yes, its true");
+}
+else{
+    console.log("nop, its false");
+}
