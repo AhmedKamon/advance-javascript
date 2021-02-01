ab5ab1e165c727ae798daacad6590bd228ac30ac
AhmedKamon <ahmedkamon3@gmail.com>
2021-02-02 01:25:45 +0600

    story of equal

---
+++ equal.js
@@ -0,0 +1,14 @@
+//two types of equal == , ===
+// == is kind of lose it will check the value to compair the value 
+// === is strict ir will check the value and its type also .
+// false is also more like o' 
+
+const numb = 10;
+// const numb1 = "10";
+const numb1 = "10";
+if(numb === numb1){
+    console.log("true");
+}
+else{
+    console.log("nop, its false");
+}
